46ec68519e9fe0ab4520e7224dfdf907ce81e4d3
Ivan Ig <105796894+HangmanSolosys@users.noreply.github.com>
2024-05-21 10:04:43 +0300

    Update pythonforansible.py

--- ansiblework/pythonforansible.py
+++ ansiblework/pythonforansible.py
@@ -3,11 +3,11 @@ import subprocess
 
 def gather_facts():
     try:
-        # Run the Ansible command to gather facts and redirect the output to a file
+       
         with open('facts.txt', 'w') as output_file:
             result = subprocess.run(['ansible', 'all', '-m', 'gather_facts'], stdout=output_file, stderr=subprocess.PIPE)
         
-        # Check the return code to determine if the command was successful
+        
         if result.returncode == 0:
             print("Ansible facts stored into 'facts.txt' file")
         else:
@@ -16,6 +16,6 @@ def gather_facts():
     except Exception as err:
         print(f"An unexpected error occurred: {err}")
 
-# Call the function to execute the Ansible command and gather facts
+
 gather_facts()
 
